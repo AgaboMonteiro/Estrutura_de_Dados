37d8c746b8f66dd37780b1048e8554044ff44132
AgaboMonteiro <agabodiwsmo@gmail.com>
2026-04-01 09:48:01 -0300

    Atividade 6 - EaD

--- filaAtendimentoModelo2026/controller/filaController.js
+++ filaAtendimentoModelo2026/controller/filaController.js
@@ -3,12 +3,24 @@
 const minhaFila = new Fila(5);
 
 function adicionarElemento() {//interação direta com o html, por isso tem o function, na classe não tem pois não existe essa interação
-  const novoElemento = document.getElementById("txtnovoNome");//colocando o nome no novoElemento
+  const inputNome = document.getElementById("txtnovoNome");//colocando o nome no novoElemento
+  const inputCpf = document.getElementById("txtCpf");//ADD CPF
 
-  if (minhaFila.enqueue(novoElemento.value)) { //se deu certo de inserir
+  const nome = inputNome.value;
+  const cpf = inputCpf.value;
+
+  if (!nome || !cpf) {//necessário preencher nome e cpf se um dos dois estiver vazio dá o aviso
+    alert("Preencha nome e CPF!");
+    return;
+  }
+
+  const atendimento = new Atendimento(nome, cpf);
+
+  if (minhaFila.enqueue(atendimento)) {//se deu certo de inserir
     mostrarFila();
-    novoElemento.value="";
-    novoElemento.focus();
+    inputNome.value = "";
+    inputCpf.value = "";
+    inputNome.focus();
   } else {
     alert("Fila cheia!");
   }
@@ -16,9 +28,28 @@ function adicionarElemento() {//interação direta com o html, por isso tem o fu
 
 
   function mostrarFila(){
-    const filaElemento = document.getElementById("lblPessoasFila");
-    filaElemento.textContent = minhaFila.toString();
+    const filaElemento = document.getElementById("listFila");
+    //filaElemento.textContent = minhaFila.toString();
+    filaElemento.innerHTML="";
+    for (let item of minhaFila) {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+    // AGORA MOSTRA OBJETO COMPLETO
+    li.textContent = item.toString();
+
+    filaElemento.appendChild(li);
   }
+}
+
+
+    /*for(let item of minhaFila){
+      const listItem = document.createElement("li"); // para cada elemento da minha fila crio um li
+      listItem.textContent = item; // coloca o elementp da fila
+      filaElemento.appendChild(listItem); // e pega o lista item e faz parte do ul
+    }
+  }*/
+
 
   function removerElemento(){
     let removido = minhaFila.dequeue();
@@ -30,4 +61,26 @@ function adicionarElemento() {//interação direta com o html, por isso tem o fu
     }
   }
 
+  function buscarElemento(){
+    //pegar valor input
+    const buscarElemento = document.getElementById("txtnovoNome");
+    let encontrado = false;
+    let cont = 0;
+    for(let item of minhaFila){
+      cont ++;
+      if(buscarElemento.value === item){
+          alert("Encontrado na fila na posição"+ cont);
+          encontrado = true;
+        }
+      
+    }
+    if(!encontrado)
+      alert ("Não encontrado na fila");
+
+  }// fim busca
+
+  //nome
+  //cpf
+  //data e hora
+  
 
--- filaAtendimentoModelo2026/model/Fila.js
+++ filaAtendimentoModelo2026/model/Fila.js
@@ -14,51 +14,53 @@ class Fila {
   isFull() {
     //por padrão o método vai ser público
     //return this.#fim === this.#elementos.length - 1; //se o fim estiver na ultima posição retorna verdadeiro, está cheio
-    return this.#qtd === this.#elementos.length;
+    return this.#qtd === this.#elementos.length; //se a quantidade que tenho na fila é igual ao tamanho do vetor
   }
 
   isEmpty() {
     return this.#qtd === 0; // verdadeiro se quantidade for igual a zero
   }
-//Fila circular é uma estrutura de dados onde os índices se comportam de forma cíclica, reutilizando posições liberadas através de aritmética modular.
+  //Fila circular é uma estrutura de dados onde os índices se comportam de forma cíclica, reutilizando posições liberadas através de aritmética modular.
   enqueue(elemento) {
     //recebe o dado que a gente vai passar
     if (!this.isFull()) {
       //se for diferente de cheio
-      
-      if(this.#fim===this.#elementos.length-1) //Verifica se o fim está na última posição do array length =5 mas ultimo indice é 4
-        this.#fim=0;
-      else
-        this.#fim++;
+
+      if (this.#fim === this.#elementos.length - 1)
+        //Verifica se o fim está na última posição do array length =5 mas ultimo indice é 4
+        this.#fim = 0;
+      else this.#fim++;
       this.#elementos[this.#fim] = elemento; //meu vetor na posição do fim recebe elemento
       this.#qtd++; //incrementa a quantidade
       console.log(
         `enqueue: início=${this.#inicio}, fim=${this.#fim}, qtd=${this.#qtd}`, //interpolação do javascript
       );
+      console.log(this.#elementos);
       return true;
     }
     return false;
   }
 
   dequeue() {
-    if (!this.isEmpty()){//se estiver vazio
+    if (!this.isEmpty()) {
+      //se estiver vazio
       let removido = this.#elementos[this.#inicio];
 
-      if(this.#inicio===this.#elementos.length-1)//Verifica se o início da fila está na última posição do array
-        this.#inicio=0;//então volta para a posição 0
-      else
-
-      
-      this.#inicio++;
+      if (this.#inicio === this.#elementos.length - 1)
+        //Verifica se o início da fila está na última posição do array
+        this.#inicio = 0; //então volta para a posição 0
+      else this.#inicio++;
       this.#qtd--; //decrementa a quantidade
 
-        console.log(`Removido: ${removido}`);
-    console.log(
+      console.log(`Removido: ${removido}`);
+      console.log(
         `dequeue: início=${this.#inicio}, fim=${this.#fim}, qtd=${this.#qtd}`, //interpolação do javascript
       );
+      console.log(this.#elementos);
       return removido;
     }
-      return null; //retorna nulo
+
+    return null; //retorna nulo
     //return this.#elementos[this.#inicio++]; //retornando o elemento na posição inicio (++ se estiver na frente da variavel executa a linha inteira para depois fazer o incremento)
   }
 
@@ -74,10 +76,37 @@ class Fila {
 
   toString() {
     let resultado = "";
-    for (let i = this.#inicio; i <= this.#fim; i++) { //do inicio até menor ou igual o fim
-      resultado += `${this.#elementos[i]} | `;//interpolação de string por |
+    let i = this.#inicio; //pega o inicio
+    for (let cont = 0; cont < this.#qtd; cont++) {
+      //
+      resultado += `${this.#elementos[i]} | `; //
+      if (i === this.#elementos.length - 1)
+        //se eu tiver na ultima posição
+        i = 0; //
+      else i++;
     }
     return resultado;
   }
 
-}
+  [Symbol.iterator]() {
+    let count = 0;
+    let i = this.#inicio; 
+    //variaveis locais, questão de escopo
+    const qtd = this.#qtd; 
+    const elementos = this.#elementos;
+    const tamanho = elementos.length;
+    return {
+      next() {
+        if (count < qtd) {//se o contador for menor q a quantidade
+          const value = elementos[i]; 
+          i = (i + 1) % tamanho; //lógica para fazer circular, sempre pega o inicio e soma mais um e divide por 5 e o resto é o novo i
+          count++;
+          return { value, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  }
+}//fim classe
+
